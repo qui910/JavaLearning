444a404c7ea10b82bb05f99f30f675a5b2d2aca1
Pangrd <qui910@163.com>
2020-06-19 10:03:11 +0800

    HashMap测试类提交

--- src/main/java/com/prd/colletctions/HashMapTest.java
+++ src/main/java/com/prd/colletctions/HashMapTest.java
@@ -1,10 +1,33 @@
 package com.prd.colletctions;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author ruidong.pang
  * @version V1.0
  * @Description TODO:功能描述
  * @date 2020-06-18 11:54
  */
+@Slf4j
 public class HashMapTest {
+
+  public static void main(String[] args) {
+      log.info("1<<1:{}",1<<1);
+      log.info("1<<4:{}",1<<4);
+
+
+      // 测试并发写 keySet
+
+
+
+      // 测试并发写 values
+
+
+
+      // 测试 SimpleEntr
+
+
+      // 测试
+
+  }
 }
