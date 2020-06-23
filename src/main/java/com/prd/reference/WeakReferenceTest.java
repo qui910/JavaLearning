36eb3b2044dd4beb35af66c9f09de3e717dfb779
Pangrd <qui910@163.com>
2020-06-23 17:20:36 +0800

    引用测试类

--- src/main/java/com/prd/reference/WeakReferenceTest.java
+++ src/main/java/com/prd/reference/WeakReferenceTest.java
@@ -2,38 +2,35 @@ package com.prd.reference;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.lang.ref.ReferenceQueue;
-import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
 
 /**
- * 软引用测试
- * 运行参数 -Xmx200m -XX:+PrintGC
+ * 弱引用测试
+ * 运行参数  -Xmx200m -XX:+PrintGC
  * 测试GC时，不能用log4j等打印日志，会增加内存消耗，测试不准确
  */
-public class SoftReferenceTest {
+@Slf4j
+public class WeakReferenceTest {
   public static void main(String[] args) throws InterruptedException {
       //100M的缓存数据
       byte[] cacheData = new byte[100 * 1024 * 1024];
-      ReferenceQueue reQueue = new ReferenceQueue();
       //将缓存数据用软引用持有
-      SoftReference<byte[]> cacheRef = new SoftReference<>(cacheData,reQueue);
-      //将缓存数据的强引用去除
-      cacheData = null;
+      WeakReference<byte[]> cacheRef = new WeakReference<>(cacheData);
       System.out.println("第一次GC前" + cacheData);
       System.out.println("第一次GC前" + cacheRef.get());
-      System.out.println("第一次GC前" + reQueue.poll());
       //进行一次GC后查看对象的回收情况
       System.gc();
       //等待GC
-      Thread.sleep(1000);
+      Thread.sleep(500);
       System.out.println("第一次GC后" + cacheData);
       System.out.println("第一次GC后" + cacheRef.get());
-      System.out.println("第一次GC后" + reQueue.poll());
 
-      //在分配一个120M的对象，看看缓存对象的回收情况
-      byte[] newData = new byte[120 * 1024 * 1024];
+      //将缓存数据的强引用去除
+      cacheData = null;
+      System.gc();
+      //等待GC
+      Thread.sleep(500);
       System.out.println("第二次GC后" + cacheData);
       System.out.println("第二次GC后" + cacheRef.get());
-      System.out.println("第二次GC后" + reQueue.poll());
   }
 }
