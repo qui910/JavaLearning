47d1a3feb90c7ef3c3a68fac883aa619f0ef7cf1
Pangrd <qui910@163.com>
2020-07-14 18:04:39 +0800

    并发测试相关代码提交

--- src/main/java/com/prd/singleton/SingleTon8.java
+++ src/main/java/com/prd/singleton/SingleTon8.java
@@ -3,6 +3,15 @@ package com.prd.singleton;
 /**
  * 枚举【推荐，生成环境中推荐】
  */
-public class SingleTon8 {
+public enum SingleTon8 {
+    INSTANCE;
 
+    public void dosomething() {
+       // 与单例无关，只是在调用单例后可以执行的方法
+        System.out.println("dosomething");
+    }
+
+    public static void main(String[] args) {
+        SingleTon8.INSTANCE.dosomething();
+    }
 }
