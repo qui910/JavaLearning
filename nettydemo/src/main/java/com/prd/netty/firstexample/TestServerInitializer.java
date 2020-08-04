2b00f0f52bef84d666b731761e1f8cb9e681fe3d
pangrd <qui910@163.com>
2020-08-04 18:38:23 +0800

    spring相关测试

---
+++ nettydemo/src/main/java/com/prd/netty/firstexample/TestServerInitializer.java
@@ -0,0 +1,20 @@
+package com.prd.netty.firstexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * 请求处理器初始化
+ */
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        // 这里必须每次new个对象
+        pipeline.addLast("HttpServerCodec",new HttpServerCodec())
+                .addLast("TestHttpServerHandler",new TestHttpServerHandler());
+    }
+}
